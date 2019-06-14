a5ebadbf21f42100e6b51aa0cfd25e1c2963a884
group003 <group003@infoway.com>
2019-06-14 22:47:21 +0530

    hello

---
+++ examProject12/src/main/java/com/example/examProject12/ExamProject12Application.java
@@ -0,0 +1,13 @@
+package com.example.examProject12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProject12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamProject12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examProject12/ExamProject12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examProject12;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class ExamProject12ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
